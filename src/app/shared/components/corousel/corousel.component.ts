0e009bb36bd09711084cfed6f90b7e00e60d739f
Luana Paes <luanafranca598@gmail.com>
2025-02-07 16:35:37 -0300

    feat(corousel): adiciona CorouselComponent com template, styles, e tests

---
+++ src/app/shared/components/corousel/corousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-corousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './corousel.component.html',
+  styleUrl: './corousel.component.scss'
+})
+export class CorouselComponent {
+
+}
