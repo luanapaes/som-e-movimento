158452cae263236336132e0186ac96c0a6606776
Luana Paes <luanafranca598@gmail.com>
2025-02-07 16:15:05 -0300

    feat(home): adiciona o HeroComponent a home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { HeroComponent } from '../../shared/components/hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    HeroComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
