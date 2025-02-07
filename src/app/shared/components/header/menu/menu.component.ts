d20979a0d63bba5485c066f86d28e628f0d0847d
Luana Paes <luanafranca598@gmail.com>
2025-02-06 22:38:42 -0300

    feat(menu): adiciona MenuComponent

---
+++ src/app/shared/components/header/menu/menu.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [
+    MatButtonModule, MatMenuModule
+  ],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+
+}
