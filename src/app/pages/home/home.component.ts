de2a360561206d2f83912d7bb1d29d6c0e484cc0
Luana Paes <luanafranca598@gmail.com>
2025-02-07 23:37:52 -0300

    feat(home): adiciona section carrossel com carrossel de imagens

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { HeroComponent } from '../../shared/components/hero/hero.component';
+import { CorouselComponent } from '../../shared/components/corousel/corousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    CorouselComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
